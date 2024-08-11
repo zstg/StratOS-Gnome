42a4d50a46f9525c923852b992ea09faeee9f90f
ZeStig <69384921+zstg@users.noreply.github.com>
2024-08-11 06:11:50 +0000

    Docker

--- build.sh
+++ build.sh
@@ -1,10 +1,61 @@
 #!/usr/bin/env bash
-echo "Cleaning up..."
-sudo rm -rf output.bak
-sudo mv -f output output.bak
-echo "Initializing ISO"
-sudo mkarchiso -v \
-     -w output \
-     -o output \
-     ./
-echo output/*.iso | awk -F/ '{print $NF}'
+
+# Function to handle errors
+handle_error() {
+    echo "Error on line $1"
+    exit 1
+}
+
+# Trap errors
+trap 'handle_error $LINENO' ERR
+[ -d /workspace ] && git config --global --add safe.directory /workspace
+
+set_environment() {
+    echo "Setting up environment..."
+    export BUILD_DATE=$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)
+    echo "BUILD_DATE=${BUILD_DATE}" >> $GITHUB_ENV
+}
+
+build() {
+    echo "Cleaning up..."
+    sudo rm -rf output.bak 2>/dev/null
+    sudo mv -f output output.bak 2>/dev/null
+    echo "Initializing ISO"
+    sudo mkarchiso -v \
+        -w output \
+        -o output \
+        ./
+    ISO_NAME=$(echo output/*.iso | awk -F/ '{print $NF}')
+    echo "Built ISO: ${ISO_NAME}"
+}
+
+create_github_release() {
+    echo "Creating GitHub release..."
+    local UPLOAD_URL
+
+    UPLOAD_URL=$(curl -s -X POST \
+        -H "Authorization: token ${GITHUB_TOKEN}" \
+        -d "{\"tag_name\": \"v${BUILD_DATE}\", \"name\": \"v${BUILD_DATE}\", \"body\": \"Automated release powered by GitHub Actions.\"}" \
+        https://api.github.com/repos/${GITHUB_REPOSITORY}/releases | jq -r .upload_url | sed -e "s/{?name,label}//")
+
+    echo "Uploading ISO to release..."
+    curl -s --data-binary @"output/${ISO_NAME}" \
+        -H "Authorization: token ${GITHUB_TOKEN}" \
+        -H "Content-Type: application/octet-stream" \
+        "${UPLOAD_URL}?name=${ISO_NAME}"
+}
+
+main() {
+    set_environment
+    build
+    create_github_release
+}
+
+# Ensure GITHUB_TOKEN is set
+if [ ! -d "/workspace" ] && [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN is not set. Please set it before running this script."
+    exit 1
+fi
+
+# Execute main function
+main
